f8eb3e865adb74014de917f332667d658d82dad3
SY-GG <sy.park.HUB@gmail.com>
2023-01-20 19:27:10 +0900

    피보나치 수 5

--- src/study20230120/B10870.java
+++ src/study20230120/B10870.java
@@ -1,4 +1,20 @@
 package study20230120;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class B10870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        System.out.println(fibonacci(input));
+    }
+    static int fibonacci(int n) {
+        if (n <= 1){
+            return n;
+        } else {
+            return fibonacci(n-2) + fibonacci(n-1);
+        }
+    }
 }
